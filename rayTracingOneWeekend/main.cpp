17d47f508a5a2f18cff561c94e18b32ffc98e232
RikkiRun <31293953+RikkiRun@users.noreply.github.com>
2018-12-20 14:21:56 -0700

    raytracing in one weekend - chacpter 2

--- rayTracingOneWeekend/main.cpp
+++ rayTracingOneWeekend/main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include <fstream>
+#include<math.h>
+#include <stdlib.h>
+#include "vec3.h"
 
 using namespace std;
+
 
 int main()
 {
@@ -20,14 +24,12 @@ int main()
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
 			//Get rgb values within range (0.0, 1.0)
 		    //Cast to integers and map to (0, 255)
-			int ir = int(255.99*r);
-			int ig = int(255.99*g);
-			int ib = int(255.99*b);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
 
 			outfile << ir << " " << ig << " " << ib << "\n";
 			std::cout << ir << " " << ig << " " << ib << "\n";
